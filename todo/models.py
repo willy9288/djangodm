7ab66d206e7151b8377a0cfc46b11af3f97acc8f
wy <wy@gmail.com>
2024-09-28 16:10:49 +0800

    增加外鍵

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Todo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     date_completed = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id}, {self.title}, {self.created}"
