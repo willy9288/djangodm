0f1dac9a35dd41719ec0af91bdadf2bcbd2f5707
wy <wy@gmail.com>
2024-09-28 14:43:25 +0800

    新增todo

--- todo/urls.py
+++ todo/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-
-# from user import views
+# from django.contrib import admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("user/", include("user.urls")),
-    path("", include("todo.urls")),
+    path("", views.todolist, name="todolist"),
 ]
