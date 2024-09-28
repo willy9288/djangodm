30155c59b4be35a55a3ee1d16048dcb73d0149bb
wy <wy@gmail.com>
2024-09-28 15:10:40 +0800

    完成資料表新增,調整 base.html顏色

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
